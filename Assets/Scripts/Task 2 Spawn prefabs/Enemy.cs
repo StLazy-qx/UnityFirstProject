1abdeb5644fdf1326e31bf0b6467d78f15bd62e7
St_L@zy <69745573+StLazy-qx@users.noreply.github.com>
2023-12-01 21:22:54 +0300

    Advanced enemy generation_2
    Now each spawn point creates its own type of enemies and has a target, that is, an object to which the enemies go. At the moment of creation, the spawn point gives its target to the enemy so that he heads towards it. There should be several such goals, each spawner has its own goal.
    
    The targets themselves move along a predetermined route, from one point to another.

--- Assets/Scripts/Task 2 Spawn prefabs/Enemy.cs
+++ Assets/Scripts/Task 2 Spawn prefabs/Enemy.cs
@@ -2,4 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour { }
+public class Enemy : MonoBehaviour 
+{
+    private float _speedMove = 5.0f;
+    private Vector3 _targetPosition;
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speedMove * Time.deltaTime);
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        _targetPosition = targetPosition;
+    }
+}
--- Assets/Scripts/Task 2 Spawn prefabs/Spawner.cs
+++ Assets/Scripts/Task 2 Spawn prefabs/Spawner.cs
@@ -1,24 +1,28 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Transform _spawnPoints;
-    [SerializeField] private Enemy _template;
-    [SerializeField] private float _timeTemplateSpawn;
-    [SerializeField] private int _lifeTime;
-    [SerializeField] private int _maxAngle;
+    [SerializeField] private Transform _targetPoints;
+    [SerializeField] private Enemy[] _templates;
 
+    private float _timeTemplateSpawn = 7.0f;
+    private int _lifeTime = 6;
     private Transform[] _points;
+    private Transform[] _targets;
 
     private void Start()
     {
         _points = new Transform[_spawnPoints.childCount];
+        _targets = new Transform[_targetPoints.childCount];
 
         for (int i = 0; i < _spawnPoints.childCount; i++)
         {
             _points[i] = _spawnPoints.GetChild(i);
+            _targets[i] = _targetPoints.GetChild(i);
         }
 
         StartCoroutine(CycleCreateEnemy());
@@ -28,12 +32,13 @@ public class Spawner : MonoBehaviour
     {
         while (true)
         {
-            int randomIndex = UnityEngine.Random.Range(0, _spawnPoints.childCount);
-            float randomAngle = UnityEngine.Random.Range(0, _maxAngle);
-            var newTemplate = Instantiate(_template,_points[randomIndex].position, 
-                Quaternion.Euler(0, randomAngle, 0));
+            for (int i = 0; i < _points.Length; i++)
+            {
+                var newTemplate = Instantiate(_templates[i], _points[i].position, Quaternion.identity);
+                newTemplate.SetTargetPosition(_targets[i].position);
 
-            Destroy(newTemplate.gameObject, _lifeTime);
+                Destroy(newTemplate.gameObject, _lifeTime);
+            }
 
             yield return new WaitForSeconds(_timeTemplateSpawn);
         }
